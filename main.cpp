97b2be2514ecfd3b999421c53e0784b2fc7c8279
Diego <diegulog@gmail.com>
2020-08-21 16:13:45 +0200

    remove zip

--- main.cpp
+++ main.cpp
@@ -26,8 +26,6 @@
 #include "lib/rlottie/inc/rlottie.h"
 #include "../examples/unicode.h"
 #include "../examples/example_util.h"
-#include <fstream>      // std::ifstream, std::ofstream
-#include <zlib.h>
 
 #define CHUNK 16384
 
@@ -69,93 +67,6 @@ bool tgsFile(std::string fileName) {
    invalid or incomplete, Z_VERSION_ERROR if the version of zlib.h and
    the version of the library linked do not match, or Z_ERRNO if there
    is an error reading or writing the files. */
-int decompress() {
-    FILE *source = WFOPEN("/Users/diegulog/1696/3.gz", "r");;
-    FILE *dest = WFOPEN("/Users/diegulog/1696/3.json", "w+");;
-    int ret;
-    unsigned have;
-    z_stream strm;
-    unsigned char in[CHUNK];
-    unsigned char out[CHUNK];
-
-    /* allocate inflate state */
-    strm.zalloc = Z_NULL;
-    strm.zfree = Z_NULL;
-    strm.opaque = Z_NULL;
-    strm.avail_in = 0;
-    strm.next_in = Z_NULL;
-    ret = inflateInit(&strm);
-    if (ret != Z_OK)
-        return ret;
-
-    /* decompress until deflate stream ends or end of file */
-    do {
-        strm.avail_in = fread(in, 1, CHUNK, source);
-        if (ferror(source)) {
-            (void) inflateEnd(&strm);
-            return Z_ERRNO;
-        }
-        if (strm.avail_in == 0)
-            break;
-        strm.next_in = in;
-
-        /* run inflate() on input until output buffer not full */
-        do {
-            strm.avail_out = CHUNK;
-            strm.next_out = out;
-            ret = inflate(&strm, Z_NO_FLUSH);
-            assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
-            switch (ret) {
-                case Z_NEED_DICT:
-                    ret = Z_DATA_ERROR;     /* and fall through */
-                case Z_DATA_ERROR:
-                case Z_MEM_ERROR:
-                    (void) inflateEnd(&strm);
-                    return ret;
-            }
-            have = CHUNK - strm.avail_out;
-            if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
-                (void) inflateEnd(&strm);
-                return Z_ERRNO;
-            }
-        } while (strm.avail_out == 0);
-
-        /* done when inflate() says it's done */
-    } while (ret != Z_STREAM_END);
-
-    /* clean up and return */
-    (void) inflateEnd(&strm);
-    return ret == Z_STREAM_END ? Z_OK : Z_DATA_ERROR;
-}
-
-void zerr(int ret) {
-    fputs("zpipe: ", stderr);
-    switch (ret) {
-        case Z_ERRNO:
-            if (ferror(stdin))
-                fputs("error reading stdin\n", stderr);
-            if (ferror(stdout))
-                fputs("error writing stdout\n", stderr);
-            break;
-        case Z_STREAM_ERROR:
-            fputs("invalid compression level\n", stderr);
-            break;
-        case Z_DATA_ERROR:
-            fputs("invalid or incomplete deflate data\n", stderr);
-            break;
-        case Z_MEM_ERROR:
-            fputs("out of memory\n", stderr);
-            break;
-        case Z_VERSION_ERROR:
-            fputs("zlib version mismatch!\n", stderr);
-    }
-}
-
-enum {
-    METADATA_ICC = (1 << 0),
-    METADATA_XMP = (1 << 1),
-    METADATA_ALL = METADATA_ICC | METADATA_XMP
-};
 
 //------------------------------------------------------------------------------
 
@@ -292,14 +203,14 @@ int main(int argc, const char *argv[]) {
 
 
     if (tgsFile(in_file)) {
-        ok = decompress();
+   /*     ok = decompress();
         if (ok != Z_OK) {
             zerr(ok);
             Help();
             goto End;
         } else {
             player = rlottie::Animation::loadFromData(in_file, in_file);
-        }
+        }*/
     } else if (jsonFile(in_file)) {
         player = rlottie::Animation::loadFromFile(in_file);
     } else {
